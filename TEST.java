fe7a5d4edbbf15a95a898243b16f9995e706b237
CHANDAN-561 <chandansahuedu@gmail.com>
2025-10-09 22:16:47 +0530

    Optimized

--- TEST.java
+++
@@ -1,7 +0,0 @@
-public class TEST {
-    public static void main(String[] args) {
-        // int[] values = new int[5];
-        int numbers[] = { 4, 0, 6, 5, 2 };
-        System.out.println(numbers[numbers.length - 1]);
-    }
-}
