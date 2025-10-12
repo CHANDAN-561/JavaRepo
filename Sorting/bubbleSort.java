a98b20ed3fba09d7a8891158e05a02e8c64fd214
CHANDAN-561 <chandansahuedu@gmail.com>
2025-10-12 14:51:41 +0530

    Optimized

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -22,7 +22,7 @@ class bubbleSort {
     public static void sort (int numbers[]) {
         int temp;
         for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1; j++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
                 if (numbers[j] > numbers[j+1]) {
                     temp = numbers[j];
                     numbers[j] = numbers[j+1];
