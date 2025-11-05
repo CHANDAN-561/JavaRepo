2f30e50423346ac85806d97e8b9c890c109f4683
CHANDAN-561 <chandansahuedu@gmail.com>
2025-11-05 12:39:40 +0530

    add binary search

--- Searching/binarySearch.java
+++ Searching/binarySearch.java
@@ -18,7 +18,7 @@ class binarySearch {
     }
 
     public static void performSort(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] > numbers[j]) {
                     int temp = numbers[i];
