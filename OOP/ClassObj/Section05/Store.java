576f04398573a7afe0b2c8f4d1d4caf76d912635
CHANDAN-561 <chandansahuedu@gmail.com>
2026-03-16 12:58:39 +0530

    use case of the modified equals() method

---
+++ OOP/ClassObj/Section05/Store.java
@@ -0,0 +1,39 @@
+package OOP.ClassObj.Section05;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Store {
+    private ArrayList<Book> books;
+
+    public Store() {
+        this.books = new ArrayList<>();
+    }
+
+    // getter & setter...
+    public Book getBook(int index) {
+        return new Book(this.books.get(index)); // deep copy
+    }
+
+    public void setBook(int index, Book bookToSet) {
+        this.books.set(index, new Book(bookToSet));
+    }
+
+    public void addBook(Book newBook) {
+        books.add(new Book(newBook));
+    }
+
+    public boolean contains(Book book) {
+        return this.books.contains(book);
+    }
+
+    public void sellBook(String title) {
+        for (int i = 0; i < this.books.size(); i++) {
+            if (this.books.get(i).getTitle() == title) {
+                this.books.remove(i);
+            } else {
+                System.out.println("The title " + title + " doesn't exist.");
+            }
+        }
+    }
+}
